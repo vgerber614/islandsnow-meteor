adcd1083dcc2e564dcfac0cb5040edd9cb901147
vgerber614 <43424164+vgerber614@users.noreply.github.com>
2019-03-03 23:30:54 -1000

    Added components.

--- app/imports/ui/components/IslandSnow.jsx
+++ app/imports/ui/components/IslandSnow.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import TopMenu from '../components/TopMenu';
 import IslandSnowLogo from '../components/IslandSnowLogo';
 import MiddleMenu from '../components/MiddleMenu';
-import FooterMenu from '../components/FooterMenu';
+import FullWidthImage from '../components/FullWidthImage';
+
 
 export default class IslandSnow extends React.Component {
 
   render() {
     return (
         <div>
-          <TopMenu/>
           <IslandSnowLogo/>
           <MiddleMenu/>
           <FullWidthImage/>
-          <FooterMenu/>
         </div>
     );
   }
